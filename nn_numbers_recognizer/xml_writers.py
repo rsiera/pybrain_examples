83a9a294a43cba8d7d8f48c820fda235697697a5
radoslaw.sieradzki <radoslaw.sieradzki@grandparade.co.uk>
2016-05-19 20:09:46 +0200

    Removed duplication

--- nn_numbers_recognizer/xml_writers.py
+++ nn_numbers_recognizer/xml_writers.py
@@ -6,6 +6,7 @@ import os
 from pybrain.tools.xml import NetworkWriter
 
 from pybrain_examples.nn_numbers_recognizer.constants import NETWORKS_DIR
+from pybrain_examples.nn_numbers_recognizer.utils import _ensure_dir
 
 
 class NeuralNetworkWriter(object):
@@ -22,10 +23,5 @@ class NeuralNetworkWriter(object):
         today = datetime.date.today().strftime("%Y_%m_%d")
         network_path = os.path.join(NETWORKS_DIR, today)
         network_name = '%s_%s.xml' % (self.trainer.totalepochs, self.hidden_neurons)
-        self._ensure_dir(network_path)
+        _ensure_dir(network_path)
         return os.path.join(network_path, network_name)
-
-    @classmethod
-    def _ensure_dir(cls, network_path):
-        if not os.path.exists(network_path):
-            os.makedirs(network_path)
